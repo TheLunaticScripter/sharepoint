9524542d181a9516d76b03e7f702be2d7a848e4a
John Snow <thelunaticscripter@outlook.com>
2017-07-13 17:05:48 -0400

    Move services to own resource
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- resources/apps_n_services.rb
+++ resources/apps_n_services.rb
@@ -1,8 +1,26 @@
 property :name, kind_of: String, name_property: true
 property :setup_acct, kind_of: String, required: true
 property :setup_pswd, kind_of: String, required: true
+property :svc_pool_acct, kind_of: String, required: true
+property :svc_pool_pswd, kind_of: String, required: true
+property :log_path, kind_of: String, default: 'C:\\SPLogs'
+property :db_name, kind_of: String, default: "#{new_resource.name}_db"
+property :usage_log_dir, kind_of: String, default: 'C:\\UsageLogs'
+property :dist_cache_sizemb, kind_of: Integer, default: 1024
+property :dist_cache_firewall_rule, kind_of: [TrueClass, FalseClass], default: false
+property :cache_provision_order, kind_of: Array
+property :app_pool_name, kind_of: String, default: 'SharePoint Service Applications'
+property :my_site_url, kind_of: String
+property :profile_db_name, kind_of: String
+property :social_db_name, kind_of: String
+property :sync_db_name, kind_of: String
+property :enable_netbios, kind_of: [TrueClass, FalseClass], default: false
+property :farm_acct, kind_of: String, required: true
+property :farm_pswd, kind_of: String, required: true
+property :audit_enable, kind_of: [TrueClass, FalseClass], default: true
+property :audit_log_size, kind_of: Integer, default: 30
 
-default_action :create
+default_ation :create_app
 
 def load_current_resource
   @current_resource = Chef::Resource::SharepointAppsNServices.new(@new_resource.name)
@@ -12,6 +30,182 @@ def whyrun_supported?
   true
 end
 
-action :create do
-  puts 'Creates the site collection'
+action :create_app do
+  dsc_resource 'ServicePoolManagedAccount' do
+    resource :SPManagedAccount
+    property :Ensure, 'Present'
+    property :AccountName, new_resource.svc_pool_acct
+    property :Account, ps_credential(new_resource.svc_pool_acct, new_resource.svc_pool_pswd)
+    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+  end
+  dsc_resource 'ServiceAppPool' do
+    resource :SPServiceAppPool
+    property :Ensure, 'Present'
+    property :ServiceAccount, new_resource.svc_pool_acct
+    property :Name, new_resource.app_pool_name
+    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+  end
+  case new_resource.name
+  when 'UsageApplication'
+    dsc_resource 'UsageApplication' do
+      resource :SPUsageApplication
+      property :Ensure, 'Present'
+      property :Name, 'Usage Service Application'
+      property :DatabaseName, new_resource.db_name
+      property :UsageLogCutTime, 5
+      property :UsageLogLocation, new_resource.usage_log_dir
+      property :UsageLogMaxFileSizeKB, 1024
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'StateServiceApp'
+    dsc_resource 'StateServiceApp' do
+      resource :SPStateServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'State Service Application'
+      property :DatabaseName, new_resource.db_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'AppManagementServiceApp'
+    dsc_resource 'AppManagementServiceApp' do
+      resource :SPAppManagmentServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'App Management Service Application'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :DatabaseName, new_resource.db_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'BCSServiceApp'
+    dsc_resource 'BCSServiceApp' do
+      resource :SPBCSServiceApp
+      property :Ensure, 'Present'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :DatabaseName, new_resource.db_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'ExcelServiceApp'
+    dsc_resource 'ExcelServiceApp' do
+      resource :SPExcelServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'Excel Services Service Application'
+      property :ApplicationPool, new_resource.app_pool_name
+    end
+  when 'ManagedMetaDataServiceApp'
+    dsc_resource 'ManagedMetaDataServiceApp' do
+      resource :SpManagedMetaDataServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'Managed Metadata Service Application'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :DatabaseName, new_resource.db_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'SearchServiceApp'
+    dsc_resource 'SearchServiceApp' do
+      resource :SPSearchServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'Search Service Application'
+      property :DatabaseName, new_resource.db_name
+      property :ApplicationPool, new_resource.app_pool_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'SecureStoreServiceApp'
+    dsc_resource 'SecureStoreServiceApp' do
+      resource :SPSecureStoreServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'Secure Store Service Application'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :DatabaseName, new_resource.db_name
+      property :AuditingEnabled, new_resource.audit_enable
+      property :AuditlogMaxSize, new_resource.audit_log_size
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'SubscriptionSettingsServiceApp'
+    dsc_resource 'SubscriptionSettingsServiceApp' do
+      resource :SPSubscriptionSettingsServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'Subscription Settings Service Application'
+      property :DatabaseName, new_resource.db_name
+      property :ApplicationPool, new_resource.app_pool_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'UserProfileServiceApp'
+    dsc_resource 'UserProfileServiceApp' do
+      resource :SPUserProfileServiceApp
+      property :Ensure, 'Present'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :MySiteHostLocation, new_resource.my_site_url
+      property :ProfileDBName, new_resource.profile_db_name
+      property :SocialDBName, new_resource.social_db_name
+      property :SyncDBName, new_resource.sync_db_name
+      property :EnableNetBIOS, new_resource.enable_netbios
+      property :FarmAccount, ps_credential(new_resource.farm_acct, new_resource.farm_pswd)
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'VisioServiceApp'
+    dsc_resource 'VisioServiceApp' do
+      resource :SPVisioServiceApp
+      property :Ensure, 'Present'
+      property :Name, 'Visio Graphics Service Application'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'WordAutomationServiceApp'
+    dsc_resource 'WordAutomationServiceApp' do
+      resource :SPWordAutomationServiceApp
+      property :Name, 'Word Automation Service Application'
+      property :Ensure, 'Present'
+      property :ApplicationPool, new_resource.app_pool_name
+      property :DatabaseName, new_resource.db_name
+      property :SupportedFileFormats, %w(docx doc mht rtf xml)
+      property :DisableEmbeddedFonts, false
+      property :MaximumMemoryUsage, 100
+      property :RecycleThreshold, 100
+      property :DisableBinaryFileScan, false
+      property :ConversionProcesses, 8
+      property :JobConversionFrequency, 15
+      property :NumberOfConversionsPerProcess, 12
+      property :TimeBeforeConversionIsMonitored, 5
+      property :MaximumConversionAttempts, 2
+      property :MaximumSyncConversionRequests, 25
+      property :KeepAliveTimeout, 30
+      property :MaximumConversionTime, 300
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  end
+end
+
+action :create_svc do
+  case new_resource.name
+  when 'DistributedCacheService'
+    dsc_resource 'ServicePoolManagedAccount' do
+      resource :SPManagedAccount
+      property :Ensure, 'Present'
+      property :AccountName, new_resource.svc_pool_acct
+      property :Account, ps_credential(new_resource.svc_pool_acct, new_resource.svc_pool_pswd)
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+    dsc_resource 'DistributedCacheService' do
+      resource :SPDistributedCacheService
+      property :Ensure, 'Present'
+      property :Name, 'AppFabricCachingService'
+      property :CacheSizeInMB, new_resource.dist_cache_sizemb
+      property :ServiceAccount, new_resource.svc_pool_acct
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+      property :CreateFirewallRules, new_resource.dist_cache_firewall_rule
+      property :ServerProvisionOrder, new_resource.cache_provision_order if new_resource.cache_provision_order
+    end
+  when 'BCSServiceInstance'
+    dsc_resource 'BCSServiceInstance' do
+      resource :SPServiceInstance
+      property :Ensure, 'Present'
+      property :Name, 'Business Data Connectivity Service'
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  when 'ManagedMetadataServiceInstance'
+    dsc_resource 'ManagedMetadataServiceInstance' do
+      resource :SPServiceInstance
+      property :Ensure, 'Present'
+      property :Name, 'Managed Metadata Web Service'
+      property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
+    end
+  end
 end
--- resources/farm.rb
+++ resources/farm.rb
@@ -10,15 +10,7 @@ property :admin_db, kind_of: String, default: 'SP_Admin'
 property :run_central_admin, kind_of: [TrueClass, FalseClass], default: true
 property :central_admin_port, kind_of: Integer, default: 5000
 property :central_admin_auth, kind_of: String, default: 'NTLM'
-property :svc_pool_acct, kind_of: String, required: true
-property :svc_pool_pswd, kind_of: String, required: true
 property :log_path, kind_of: String, default: 'C:\\SPLogs'
-property :usage_db, kind_of: String, default: 'SP_Usage'
-property :usage_log_dir, kind_of: String, default: 'C:\\UsageLogs'
-property :state_svc_db, kind_of: String, default: 'SP_State'
-property :dist_cache_sizemb, kind_of: Integer, default: 1024
-property :dist_cache_firewall_rule, kind_of: [TrueClass, FalseClass], default: false
-property :cache_provision_order, kind_of: Array
 
 default_action :create
 
@@ -45,13 +37,6 @@ action :create do
     property :CentralAdministrationAuth, new_resource.central_admin_auth if new_resource.run_central_admin
     timeout 1500
   end
-  dsc_resource 'ServicePoolManagedAccount' do
-    resource :SPManagedAccount
-    property :Ensure, 'Present'
-    property :AccountName, new_resource.svc_pool_acct
-    property :Account, ps_credential(new_resource.svc_pool_acct, new_resource.svc_pool_pswd)
-    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
-  end
   dsc_resource 'ApplyDiagnosticLogSettings' do
     resource :SPDiagnosticLoggingSettings
     property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
@@ -74,33 +59,6 @@ action :create do
     property :ScriptErrorReportingEnabled, true
     property :ScriptErrorReportingRequireAuth, true
   end
-  dsc_resource 'UsageApplication' do
-    resource :SPUsageApplication
-    property :Ensure, 'Present'
-    property :Name, 'Usage Service Application'
-    property :DatabaseName, new_resource.usage_db
-    property :UsageLogCutTime, 5
-    property :UsageLogLocation, new_resource.usage_log_dir
-    property :UsageLogMaxFileSizeKB, 1024
-    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
-  end
-  dsc_resource 'StateServiceApp' do
-    resource :SPStateServiceApp
-    property :Ensure, 'Present'
-    property :Name, 'State Service Application'
-    property :DatabaseName, new_resource.state_svc_db
-    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
-  end
-  dsc_resource 'EnableDistributedCache' do
-    resource :SPDistributedCacheService
-    property :Ensure, 'Present'
-    property :Name, 'AppFabricCachingService'
-    property :CacheSizeInMB, new_resource.dist_cache_sizemb
-    property :ServiceAccount, new_resource.svc_pool_acct
-    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
-    property :CreateFirewallRules, new_resource.dist_cache_firewall_rule
-    property :ServerProvisionOrder, new_resource.cache_provision_order if new_resource.cache_provision_order
-  end
 end
 
 action :join do
@@ -113,15 +71,5 @@ action :join do
     property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
     property :AdminContentDatabaseName, new_resource.admin_db if new_resource.run_central_admin
     property :RunCentralAdmin, new_resource.run_central_admin
-  end
-  dsc_resource 'EnableDistributedCache' do
-    resource :SPDistributedCacheService
-    property :Ensure, 'Present'
-    property :Name, 'AppFabricCachingService'
-    property :CacheSizeInMB, new_resource.dist_cache_sizemb
-    property :ServiceAccount, new_resource.svc_pool_acct
-    property :PsDscRunAsCredential, ps_credential(new_resource.setup_acct, new_resource.setup_pswd)
-    property :CreateFirewallRules, new_resource.dist_cache_firewall_rule
-    property :ServerProvisionOrder, new_resource.cache_provision_order if new_resource.cache_provision_order
   end
 end
